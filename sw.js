0c65c8e545293782bac210bf8706f4a2de80d7d1
J1800 <josephh90@gmail.com>
2025-04-26 02:17:19 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install',e=>{
+    e.waitUntil(
+        caches.open('offline-v1').then(c=>c.add('eventscript42.html'))
+    )
+})
+self.addEventListener('fetch',e=>{
+    const url=new URL(e.request.url)
+    if(url.pathname.endsWith('eventscript42.html')){
+        e.respondWith(
+            caches.match('eventscript42.html').then(r=>r||fetch(e.request))
+        )
+    }
+})
+
